22242d2eae20cf1865c05e3122071ab974426a98
Richard_cyber <128294864+rihard-cyber@users.noreply.github.com>
2026-06-11 14:32:14 +0700

    chore: production build sync

--- docs/assets/phone-C_ZRBKdy.js
+++ docs/assets/phone-C_ZRBKdy.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-SycflM4G.js";/**
+import{c as a}from"./index-Bp5ZWIK2.js";/**
  * @license lucide-react v0.395.0 - ISC
  *
  * This source code is licensed under the ISC license.
